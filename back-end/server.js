785de5b0c1a1bd29be0739226e92e68e6991a7be
KhoaNguyen <78020093+khoadeu25890@users.noreply.github.com>
2022-10-14 00:34:38 +0700

    test-commit-be

--- back-end/server.js
+++ back-end/server.js
@@ -2,6 +2,6 @@ import express from "express";
 const app = express();
 
 const port = process.env.PORT || 5000;
-console.log("123");
+console.log("Hello World");
 
 app.listen(port, () => console.log(`Server Running Port ${port}`));
