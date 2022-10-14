9b67b468fc953323d45ddbb6176018b8c21b71e0
KhoaNguyen <78020093+khoadeu25890@users.noreply.github.com>
2022-10-14 12:58:50 +0700

    XUON-4  setup & connect database

---
+++ back-end/database/db.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(
+      "mongodb+srv://khoangu123:khoangu123@xuongda.xiuf6w1.mongodb.net/proshop?retryWrites=true&w=majority ",
+      {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
+      }
+    );
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
