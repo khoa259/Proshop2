9fbc493d5e15bd04b1b0d81e4f570a38fd9f456e
Tu Nguyen <nguyenminhtuk52@gmail.com>
2022-10-14 21:23:51 +0700

    ft:crud products nodejs

---
+++ back-end/model/product.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from 'mongoose';
+const productSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    decription: {
+        type: String,
+        required: true
+    },
+});
+export default mongoose.model('Product', productSchema);
