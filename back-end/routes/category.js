b13d06472b75bf32904d8a5f04230df442cfbce5
manhkkkk <79675048+manhkkkk@users.noreply.github.com>
2022-10-22 20:49:15 +0700

    update

--- back-end/routes/category.js
+++ back-end/routes/category.js
@@ -1,11 +1,10 @@
 import { Router } from "express";
 import { create, read, list, remove, update } from "../controller/category.js";
 
-const authRouter = Router();
-authRouter.post("/category", create);
-authRouter.get("/category/:id", read);
-authRouter.get("/category", list);
-authRouter.delete("/category/:id", remove);
-authRouter.put("/category/:id", update);
+const router = Router();
+router.get("/:id", read);
+router.get("/", list);
+router.delete("/:id", remove);
+router.put("/:id", update);
 
 export default router;
