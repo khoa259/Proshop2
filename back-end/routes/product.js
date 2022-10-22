960acdc8f6869f22dd669a12132815f873717635
manhkkkk <79675048+manhkkkk@users.noreply.github.com>
2022-10-22 19:31:34 +0700

    BE-products-search

--- back-end/routes/product.js
+++ back-end/routes/product.js
@@ -2,14 +2,14 @@
 import express from 'express';
 import { Router } from 'express';
 import upload from '../middleware/multer.js';
-import { create, update, remove, readAll, read } from '../controller/product.js'
+import { create, update, remove, list, read } from '../controller/product.js'
 const router = Router();
 
 router.post(
     '/',
     create
 );
-router.get('/', readAll);
+router.get('/', list);
 router.get('/:productId', read);
 router.put(
     '/:productId',
