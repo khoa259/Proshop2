bedf4007ee0b1d0dd0866fe4cd13c8f2169cd183
manhkkkk <79675048+manhkkkk@users.noreply.github.com>
2022-10-22 19:34:37 +0700

    update search products

--- back-end/routes/product.js
+++ back-end/routes/product.js
@@ -1,19 +1,12 @@
-
-import express from 'express';
-import { Router } from 'express';
-import upload from '../middleware/multer.js';
-import { create, update, remove, list, read } from '../controller/product.js'
+import express from "express";
+import { Router } from "express";
+import upload from "../middleware/multer.js";
+import { create, update, remove, list, read } from "../controller/product.js";
 const router = Router();
 
-router.post(
-    '/',
-    create
-);
-router.get('/', list);
-router.get('/:productId', read);
-router.put(
-    '/:productId',
-    update
-);
-router.delete('/:productId', remove);
+router.post("/", create);
+router.get("/", list);
+router.get("/:productId", read);
+router.put("/:productId", update);
+router.delete("/:productId", remove);
 export default router;
