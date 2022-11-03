4b4ce52305a3ad92f7e49763e22b10269bd3ae20
manhkkkk <79675048+manhkkkk@users.noreply.github.com>
2022-11-03 08:39:44 +0700

    UI-client-cart

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -14,8 +14,8 @@ import EditProduct from "./pages/admin/ProductManage/EditProduct";
 import ListProduct from "./pages/admin/ProductManage/ListProduct";
 import Home from "./pages/client/Home";
 import Shop from "./pages/client/Shop";
-// import Blog from "./pages/client/Blog";
-// import Cart from "./pages/client/Cart";
+import Blog from "./pages/client/Blog";
+import Cart from "./pages/client/Cart";
 // import CheckOut from "./pages/client/CheckOut";
 // import Contact from "./pages/client/Contact";
 // import ShopDetail from "./pages/client/ShopDetail";
@@ -27,10 +27,10 @@ const App = () => {
       <Route path="/" element={<WebsiteLayout />}>
           <Route index element={<Home />}/>
           <Route path="shop" element={<Shop/>}  />
-          {/* <Route path="products/detail" element={<ShopDetail/>}  />
           <Route path="cart" element={<Cart/>}  />
-          <Route path="checkout" element={<CheckOut/>}  />
           <Route path="blog" element={<Blog/>}  />
+          {/* <Route path="products/detail" element={<ShopDetail/>}  />
+          <Route path="checkout" element={<CheckOut/>}  />
           <Route path="contact" element={<Contact/>}  />
           <Route path="login" element={<Signin/>}  />
           <Route path="signup" element={<Signup/>}  /> */}
