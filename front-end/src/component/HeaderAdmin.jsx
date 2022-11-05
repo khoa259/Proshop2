296f3ab950672544abd7ccacf2acdcbdf14982f9
Tu Nguyen <nguyenminhtuk52@gmail.com>
2022-11-05 13:23:18 +0700

    ft:list product admin

--- front-end/src/component/HeaderAdmin.jsx
+++ front-end/src/component/HeaderAdmin.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 const HeaderAdmin = () => {
     return (
         <div>
@@ -16,21 +16,19 @@ const HeaderAdmin = () => {
                 {/* Sidebar menu*/}
                 <div className="app-sidebar__overlay" data-toggle="sidebar" />
                 <aside className="app-sidebar">
-                    <div className="app-sidebar__user"><img className="app-sidebar__user-avatar" src="/images/hay.jpg" width="50px" alt="User Image" />
+                    <div className="app-sidebar__user"><img style={{ marginLeft: "70px" }} className="app-sidebar__user-avatar" src="https://picsum.photos/200" width="50px" alt="User Image" />
                         <div>
-                            <p className="app-sidebar__user-name"><b>Võ Trường</b></p>
+                            <p className="app-sidebar__user-name"><b>Nhóm 1</b></p>
                             <p className="app-sidebar__user-designation">Chào mừng bạn trở lại</p>
                         </div>
                     </div>
                     <hr />
                     <ul className="app-menu">
-                        <li><a className="app-menu__item haha" href="phan-mem-ban-hang.html"><i className="app-menu__icon bx bx-cart-alt" />
-                            <span className="app-menu__label">POS Bán Hàng</span></a></li>
-                        <li><a className="app-menu__item active" href="index.html"><i className="app-menu__icon bx bx-tachometer" /><span className="app-menu__label">Bảng điều khiển</span></a></li>
+                        <Link to=""><li><a className="app-menu__item active" href="index.html"><i className="app-menu__icon bx bx-tachometer" /><span className="app-menu__label">Bảng điều khiển</span></a></li></Link>
                         <li><a className="app-menu__item " href="table-data-table.html"><i className="app-menu__icon bx bx-id-card" /> <span className="app-menu__label">Quản lý nhân viên</span></a></li>
                         <li><a className="app-menu__item" href="#"><i className="app-menu__icon bx bx-user-voice" /><span className="app-menu__label">Quản lý khách hàng</span></a></li>
-                        <li><a className="app-menu__item" href="table-data-product.html"><i className="app-menu__icon bx bx-purchase-tag-alt" /><span className="app-menu__label">Quản lý sản phẩm</span></a>
-                        </li>
+                        <Link to='product'><li><a className="app-menu__item" href="table-data-product.html"><i className="app-menu__icon bx bx-purchase-tag-alt" /><span className="app-menu__label">Quản lý sản phẩm</span></a>
+                        </li></Link>
                         <li><a className="app-menu__item" href="table-data-oder.html"><i className="app-menu__icon bx bx-task" /><span className="app-menu__label">Quản lý đơn hàng</span></a></li>
                         <li><a className="app-menu__item" href="table-data-oder.html"><i className="app-menu__icon bx bx-category" /><span className="app-menu__label">Quản lý danh mục</span></a></li>
 
